730a9180498820fb6fdbf26f179d2bc902c424ea
exec888 <117951720+exec888@users.noreply.github.com>
2022-11-16 22:44:50 +0500

    Update nil1.lua

--- nil1.lua
+++ nil1.lua
@@ -103,13 +103,13 @@ Sect:AddToggle({
 	end    
 })
 local dn = game.Players.LocalPlayer.Character.HumanoidRootPart.Size
-local tog2 = false
+_G.tog2 = false
 Sect:AddToggle({
 	Text = "HITBOX EXPANDER",
 	Key = "2",
 	Default = false,
 	Callback = function(Value)
-		tog2 = Value
+		_G.tog2 = Value
 	end
 })
 local tog3
@@ -427,6 +427,27 @@ misc:AddButton({
 			return Target
 		end
 		game:GetService("ReplicatedStorage").Events.InteractEvent:FireServer(GetClosestVehicle().VehicleSeat);
+	end
+})
+misc:AddButton({
+	Text = "Make it Day",
+	Callback = function()
+		game.Lighting.Condition.Value = "Day"
+		game.Lighting:SetMinutesAfterMidnight(720)
+		game:GetService("Lighting").SunRays.Intensity = 0.25
+		game:GetService("Lighting").SunRays.Spread = 1
+		game:GetService("Lighting").Bloom.Intensity = 0.4
+		game:GetService("Lighting").Sky.SkyboxBk = "rbxassetid://2677179366"
+		game:GetService("Lighting").Sky.SkyboxDn = "rbxassetid://501313275"
+		game:GetService("Lighting").Sky.SkyboxFt = "rbxassetid://2677179602"
+		game:GetService("Lighting").Sky.SkyboxLf = "rbxassetid://2677179822"
+		game:GetService("Lighting").Sky.SkyboxRt = "rbxassetid://2677179738"
+		game:GetService("Lighting").Sky.SkyboxUp = "rbxassetid://247087559"
+		game:GetService("Lighting").Brightness =  1.85
+		game:GetService("Lighting").FogColor =  Color3.fromRGB(255, 247, 234)
+		game:GetService("Lighting").OutdoorAmbient =  Color3.fromRGB(185, 178, 167)
+		game:GetService("Lighting").FogEnd = 7000;
+		game:GetService("Lighting").FogStart = 0;
 	end
 })
 function GetClosestItem()
@@ -1104,7 +1125,7 @@ spawn(function()
 		else
 
 		end
-		if tog2 then
+		if _G.tog2 then
 			local Players = game.Players
 			for i, v in pairs(Players:GetChildren()) do
 				repeat wait() until game.Players.LocalPlayer:HasAppearanceLoaded()
